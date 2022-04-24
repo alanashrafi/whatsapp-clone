5f9fbcfb57a15e8c9dc43ed266526f5dee9fed3b
Alan Ashrafi <alan_1368@yahoo.com>
2022-04-24 12:43:20 +0200

    config file

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDLl-8sJyTZgm_ikhyGxGd4eEt8swxykj8',
+  authDomain: 'whatsapp-clone-b6cf2.firebaseapp.com',
+  projectId: 'whatsapp-clone-b6cf2',
+  storageBucket: 'whatsapp-clone-b6cf2.appspot.com',
+  messagingSenderId: '900655615187',
+  appId: '1:900655615187:web:ae169cd458c12e80cc5826',
+};
